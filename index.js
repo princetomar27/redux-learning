3d018e59d182e2b064575c6cc4b5d304e78e32cf
princetomar <tomarprince443@gmail.com>
2025-02-20 22:36:24 +0530

    increment and decrement and increment by amount done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
 // store
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger.default));
 
 const historyState = [];
 
@@ -10,17 +11,35 @@ function reducer(state = { amount: 1 }, action) {
   if (action.type === "increment") {
     return { amount: state.amount + 1 };
   }
+  if (action.type === "decrement") {
+    return { amount: state.amount - 1 };
+  }
+  if (action.type === "incrementByAmount") {
+    return { amount: state.amount + action.payload };
+  }
   return state;
 }
 
 // global state
-store.subscribe(() => {
-  historyState.push(store.getState());
-  console.log(historyState);
-});
+// store.subscribe(() => {
+//   historyState.push(store.getState());
+//   console.log(historyState);
+// });
+
+// setInterval(() => {
+//   store.dispatch({ type: "increment" });
+// }, 2000);
+
+// setInterval(() => {
+//   store.dispatch({ type: "decrement" });
+// }, 3000);
+
+function incrementCounter() {
+  return { type: "increment" };
+}
 
 setInterval(() => {
-  store.dispatch({ type: "increment" });
-}, 2000);
+  store.dispatch(incrementCounter());
+}, 1500);
 
 console.log(store.getState());
