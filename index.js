903eda6f13eda01c791658a0104a2fe1c51e41ea
Prince Tomar <tomarprince443@gmail.com>
2025-02-20 22:22:08 +0530

    created reducer

---
+++ index.js
@@ -0,0 +1,19 @@
+import { createStore } from "redux";
+
+// store
+const store = createStore(reducer);
+
+// reducer
+function reducer(state = { amount: 1 }, action) {
+  if (action.type === "increment") {
+    return { amount: state.amount + 1 };
+  }
+  return state;
+}
+
+// global state
+console.log(store.getState());
+
+store.dispatch({ type: "increment" });
+
+console.log(store.getState());
